6992856a04d0d31f65716f889d165ceea8e80cb5
smck0583 <smck0583@uni.sydney.edu.au>
2023-08-20 16:16:27 +1000

    Update the design of pages

--- minerva/src/App.js
+++ minerva/src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Button from "./Button";
 
+
+/*
+  This page is responsible for 'landing' the 
+  users and guiding them to start the demo 
+*/
 function LandingPage() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>Welcome to Project Minerva</h1>
           <p>Reimagine learning</p>
-          <Link to="/try">Try Here</Link>
+          <Link to="/try">
+            <Button name={"Try Here"} className={"nice-blue"} />  
+          </Link>
         </header>
       </div>
     );
 }
 
+/*
+  This is the page that handles the functionality 
+  of the app, i.e. the demo 
+*/
 function TryPage() {
   return (
     <div className="App">
@@ -25,7 +37,10 @@ function TryPage() {
   );
 }
 
-
+/*
+  Main functionality of the page, think of it 
+  like the router of a python server
+*/
 function App() {
   return (
     <Router>
