3146824a8af18812098773a212c2383af4a095f7
smck0583 <smck0583@uni.sydney.edu.au>
2023-08-20 17:08:28 +1000

    Try Page formatting

--- minerva/src/App.js
+++ minerva/src/App.js
@@ -50,40 +50,41 @@ function TryPage() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Try Page</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
+        <h1>Ask Away!</h1>
+        <form onSubmit={handleSubmit} className="form-container">
+          <label className="form-label">
             Subject:
-            <select name="subject" value={formData.subject} onChange={handleInputChange}>
+            <select name="subject" value={formData.subject} onChange={handleInputChange} className="form-select">
               <option value="math">Math</option>
               <option value="science">Science</option>
               <option value="history">History</option>
             </select>
           </label>
-          <label>
+          <label className="form-label">
             Delivery Method:
-            <select name="deliveryMethod" value={formData.deliveryMethod} onChange={handleInputChange}>
+            <select name="deliveryMethod" value={formData.deliveryMethod} onChange={handleInputChange} className="form-select">
               <option value="online">Online</option>
               <option value="in-person">In-Person</option>
             </select>
           </label>
-          <label>
+          <label className="form-label">
             Level:
-            <select name="level" value={formData.level} onChange={handleInputChange}>
+            <select name="level" value={formData.level} onChange={handleInputChange} className="form-select">
               <option value="beginner">Beginner</option>
               <option value="intermediate">Intermediate</option>
               <option value="advanced">Advanced</option>
             </select>
           </label>
-          <label>
+          <label className="form-label">
             Additional Details:
             <textarea
               name="additionalDetails"
               value={formData.additionalDetails}
               onChange={handleInputChange}
+              className="form-textarea"
             />
           </label>
-          <button type="submit">Submit</button>
+          <button type="submit" className="form-button">Submit</button>
         </form>
       </header>
     </div>
